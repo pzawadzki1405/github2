2c2faa5a1f0b969a1c381b9573602de3760513cd
PZawadzki <p.zawadzki1405@gmail.com>
2022-01-11 17:47:46 -0600

    added menu and replenishment function

--- inventoryt.py
+++ inventoryt.py
@@ -3,49 +3,93 @@
 # import openpyxl module
 import openpyxl
 
-# Give the location of the file
-path = "C:\\Users\\wareh\\OneDrive\\Masaüstü\\INVENTORY.xlsx"
+file1 = open("MyFile.txt","w")
+path = input('Please enter path of excel file')
 
+# Give the location of the file
+if (path == ''):
+    path = "C:\\Users\\wareh\\OneDrive\\Masaüstü\\INVENTORY.xlsx"
+blacklist = ['96PF250', '96PF444', '96PF251', '96GT15P', '96BOX004', '96FM003A', '96FM003P'
+            '96FM005A', '96FM005P']
 # To open the workbook
 # workbook object is created
+print('Loading the file... Please wait')
 wb_obj = openpyxl.load_workbook(path)
-
-wb_obj.active = 2
-
-# Get workbook active sheet object
-# from the active attribute
-sheet_obj = wb_obj.active
-
-# Cell objects also have a row, column,
-# and coordinate attributes that provide
-# location information for the cell.
-
-# Note: The first row or
-# column integer is 1, not 0.
-
 print(wb_obj.sheetnames)
 
-treshhold = input('Please enter minimum quantity on location: ')
+def inventory():
+    wb_obj.active = 2
+    sheet_obj = wb_obj.active
+    treshhold_min = input('Please enter minimum quantity on location: ')
 
-#sheet_obj = wb_obj.get_sheet_by_name('Pallet locations')
 
-# Cell object is created by using
-# sheet object's cell() method.
-cell_obj = sheet_obj.cell(row = 1, column = 1)
+    for i in range(1, len(list(sheet_obj.rows))):
+        cell_obj = sheet_obj.cell(row = i, column = 1)
+        cell_obj_v = cell_obj.value
+        cell_obj_next = sheet_obj.cell(row = i+1, column = 1)
+        cell_obj_next_v = cell_obj_next.value
+        if (cell_obj_v == cell_obj_next_v):
+            if (int((sheet_obj.cell(row = i, column = 4)).value) <= int(treshhold_min) ):
+                print(str(cell_obj_v) +
+                    ' at location ' + str(((sheet_obj.cell(row = i, column = 3)).value)) +
+                    ' have ' + str(((sheet_obj.cell(row = i, column = 4)).value)) + ' avaible')
+    while(True):
+        menu = input('Please enter 0 to go to main menu')
+        if (menu == '0'):
+            break
+        else:
+            print('Wrong action, please try again')
 
-for i in range(1, len(list(sheet_obj.rows))):
-    cell_obj = sheet_obj.cell(row = i, column = 1)
-    cell_obj_v = cell_obj.value
-    cell_obj_next = sheet_obj.cell(row = i+1, column = 1)
-    cell_obj_next_v = cell_obj_next.value
-    if (cell_obj_v == cell_obj_next_v):
-        if (int((sheet_obj.cell(row = i, column = 4)).value) <= int(treshhold) ):
-            print(str(cell_obj_v) +
-                ' at location ' + str(((sheet_obj.cell(row = i, column = 3)).value)) +
-                ' have ' + str(((sheet_obj.cell(row = i, column = 4)).value)) + ' avaible')
-# Print value of cell object
-# using the value attribute
+def replenishment():
+    wb_obj.active = 2
+    sheet_obj = wb_obj.active
+    treshhold_min = input('Please enter minimum quantity on location: ')
+    threshold_max = input('Please enter maximum quantity on location: ')
+    file1.write('Minimum quanttity: ' + str(treshhold_min) + '\n')
+    file1.write('Maximum quanttity: ' + str(threshold_max) + '\n')    
+    black = False
+    for i in range(1, len(list(sheet_obj.rows))):
+        cell_obj = sheet_obj.cell(row = i, column = 1)
+        cell_obj_v = cell_obj.value
+        cell_obj_next = sheet_obj.cell(row = i+1, column = 1)
+        cell_obj_next_v = cell_obj_next.value
+        if (cell_obj_v == cell_obj_next_v):
+            if (int((sheet_obj.cell(row = i, column = 4)).value) <= int(treshhold_min) ):
+                result = int((sheet_obj.cell(row = i, column = 4)).value) + int((sheet_obj.cell(row = i+1, column = 4)).value)
+                if (result <= int(threshold_max)):
+                    for j in range(0, len(blacklist)):
+                        if (blacklist[j] == str(cell_obj_v)):
+                            black = True
+                    if (black == False):
+                        message = ('Transfer ' + str(cell_obj_v) +
+                            ' from ' + str(((sheet_obj.cell(row = i, column = 3)).value)) +
+                            ' to ' + str(((sheet_obj.cell(row = i+1, column = 3)).value)) +
+                            ' it will be ' + str(result)  + ' total')
+                        print(message)
+                        file1.write((message + '\n'))
+                        black = False
 
-#print(sheets)
+    while(True):
+        menu = input('Please enter 0 to go to main menu')
+        if (menu == '0'):
+            break
+        else:
+            print('Wrong action, please try again')
 
-#print(list(sheet_obj.rows))
+while(True):
+
+    print('1 - Inventory')
+    print('2 - Replenishment')
+    print('3 - Receiving')
+    print('9 - Close application')
+    print('0 - Main menu')
+    menu = input('Please enter action:')
+    if (menu == '9'):
+        file1.close()
+        break
+    elif (menu == '1'):
+        inventory()
+    elif (menu == '2'):
+        replenishment()
+    else:
+        print('Wrong Action, please try again')
