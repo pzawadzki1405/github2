dc5dfef46ad51753e464201ab85d2d7e6c66f6aa
PZawadzki <p.zawadzki1405@gmail.com>
2022-01-29 15:20:25 -0600

    fulfill file

--- fulfill.py
+++ fulfill.py
@@ -1,12 +1,46 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
 import time
+import pickle
 
+def look_for_order(order):
+    driver.find_element_by_id('_searchstring').send_keys(order)
+    time.sleep(2)
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//li[@class='uir-global-search-item']")))
+    driver.find_element_by_xpath("//li[@class='uir-global-search-item']/a[1]").click()
+    ordernr = driver.find_element_by_xpath("//div[@class='uir-record-id']")
+    print(ordernr.text)
+
+def fulfill():
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "process")))
+    driver.find_element_by_id("process").click()
+    shipping_status = driver.find_element_by_id('inpt_shipstatus5')
+    shipping_status.click()
+    time.sleep(2)
+    driver.find_element_by_id('inpt_shipstatus5').send_keys("Packed")
+    driver.find_element_by_id('spn_multibutton_submitter').click()
+    element = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//div[@class='uir-record-status']")))
+    status = driver.find_element_by_xpath("//div[@class='uir-record-status']")
+    if(status.text == 'PACKED'):
+        time.sleep(2)
+
+orders = ['WS69974']
 driver = webdriver.Firefox()
-driver.get("https://6241176-sb1.app.netsuite.com/app/accounting/transactions/salesord.nl?&id=3212986&scrollid=3212986&whence=&cmid=1643470020809_7264")#put here the adress of your page
+driver.maximize_window()
+time.sleep(2)
+driver.get("https://6241176-sb1.app.netsuite.com/app/center/card.nl?sc=-29&whence=")#put here the adress of your page
 #elem = driver.find_elements_by_xpath("//*[@type='submit']")#put here the content you have put in Notepad, ie the XPath
 #button = driver.find_element_by_id('process') #//Or find button by ID.
 #driver.get("https://www.python.org")
+cookies = pickle.load(open("cookies.pkl", "rb"))
+for cookie in cookies:
+    driver.add_cookie(cookie)
+driver.get("https://6241176-sb1.app.netsuite.com/app/center/card.nl?sc=-29&whence=")#put here the adress of your page
+
 time.sleep(2)
 print(driver.title)
 if (driver.title == 'NetSuite Login'):
@@ -18,8 +52,14 @@ if (driver.title == 'NetSuite Login'):
 
     driver.find_element_by_id("login-submit").click()
     time.sleep(5)
+
+for order in orders:
+    look_for_order(order)
+    fulfill()
+
 while(True):
     a = input('1 aby zakonczyc: ')
     if (a=='1'):
+        pickle.dump( driver.get_cookies() , open("cookies.pkl","wb"))
         break
 driver.close()
